6f7f4b27f9112e26b625488a3995b295d2034796
Emily Morgan <emorgan05@gmail.com>
2017-10-23 21:16:12 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize(args)
+    @id = args[:id]
+    @name = args[:name]
+    @type = args[:type]
+    @db = args[:db]
+    @@all << self
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", [name, type])
+  end
+
+  def self.find(id, db)
+    array = db.execute("SELECT * FROM pokemon WHERE pokemon.id = (?)", [id])
+    p = {}
+    p[:id] = array[0][0]
+    p[:name] = array[0][1]
+    p[:type] = array[0][2]
+    p[:db] = db
+    Pokemon.new(p)
+  end
 end
+
